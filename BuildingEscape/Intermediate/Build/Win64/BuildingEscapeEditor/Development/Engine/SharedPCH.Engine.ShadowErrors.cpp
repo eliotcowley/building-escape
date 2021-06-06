f61e845eea2f5bb2bb9f7a55b1e01309785fe21b
Eliot Cowley <eliot93@live.com>
2021-06-05 17:12:39 -0700

    Started Building Escape game

---
+++ BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29915
+#include "E:/Repos/unreal-engine-projects/building-escape/BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
